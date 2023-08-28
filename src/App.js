4bac1fa8beac3eddf272099098b4713fc30eeb83
brayan salas <brayan.salas@udgvirtual.udg.mx>
2023-08-28 17:10:06 -0600

    Primer commit, creacion proyecto

---
+++ src/App.js
@@ -0,0 +1,171 @@
+function SprintTable() {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Elemento de trabajo</th>
+          <th>Pendiente</th>
+          <th>Puntos de historia</th>
+          <th>Responsable</th>
+          <th>Estado</th>
+          <th>Estimado original</th>
+          <th>Día 1</th>
+          <th>Día 2</th>
+          <th>Día 3</th>
+          <th>Día 4</th>
+          <th>Día 5</th>
+          <th>Revisión del Sprint</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>ID de la historia #1 - Diseño de la página de inicio</td>
+          <td>Tarea - Diseño de la página de inicio</td>
+          <td>2</td>
+          <td>Diseñador ui/ux</td>
+          <td>Pendiente</td>
+          <td>Definición de tareas y expectativas del cliente</td>
+          <td>Trabajo en la maqueta visual</td>
+          <td>Trabajo en imágenes</td>
+          <td>Trabajo en el diseño</td>
+          <td>Revisión y ajustes</td>
+          <td>Trabajo en la maqueta visual</td>
+          <td>Revisión de la página antes de su implementación</td>
+        </tr>
+        <tr>
+          <td>ID de la historia #2 - Menú</td>
+          <td>Dividir el menú en categorías</td>
+          <td>4</td>
+          <td>Diseñador ui/ux</td>
+          <td>Pendiente</td>
+          <td>Mostrar una demostración visual</td>
+          <td>Trabajo en categorías y tortas</td>
+          <td>Trabajo en imágenes y precios</td>
+          <td>Trabajo en opciones y personalización</td>
+          <td>Trabajo en integración y funcionamiento de menú</td>
+          <td>Revisión del menú</td>
+          <td>Entrega de producto</td>
+        </tr>
+        <tr>
+          <td>ID de la historia #3 - Información de contacto y ubicación</td>
+          <td>Mostrar imágenes para añadir autenticidad a la empresa</td>
+          <td>2</td>
+          <td>Equipo de contenido</td>
+          <td>Pendiente</td>
+          <td>Recopilación de información de contacto</td>
+          <td>Trabajo en la maqueta visual de área</td>
+          <td>Añadir datos de ubicación a área</td>
+          <td>Validación de datos de contacto</td>
+          <td>Trabajo en la maqueta visual de área</td>
+          <td>Mostrar fase de uso</td>
+          <td>Entrega de producto</td>
+        </tr>
+        <tr>
+          <td>ID de la historia #4 - Galería de imágenes</td>
+          <td>Destacar la selección de imágenes</td>
+          <td>2</td>
+          <td>Diseñador de contenido</td>
+          <td>Pendiente</td>
+          <td>Selección de imágenes de relevancia</td>
+          <td>Edición y optimización de imágenes</td>
+          <td>Trabajo en el diseño de sección de galería</td>
+          <td>Integración de imágenes en la página</td>
+          <td>Trabajo en el diseño de sección de galería</td>
+          <td>Revisión de imágenes</td>
+          <td>Entrega de producto</td>
+        </tr>
+        <tr>
+          <td>ID de la historia #5 - Integración de datos de los productos</td>
+          <td>Resaltar la autenticidad de la empresa con estilo visual y propio de la marca</td>
+          <td>5</td>
+          <td>Diseñador ui/ux</td>
+          <td>Pendiente</td>
+          <td>Diseño de boceto</td>
+          <td>Desarrollo funcional</td>
+          <td>Desarrollo funcional</td>
+          <td>Desarrollo funcional</td>
+          <td>Desarrollo funcional</td>
+          <td>Revisión y ajustes</td>
+          <td>Entrega de producto</td>
+          
+        </tr>
+      </tbody>
+      <div>
+      <div className="sprint-planning-document">
+        <h2>Documentación de Planificación del Sprint</h2>
+        <h3>Detalles del Sprint</h3>
+        <p>
+          <strong>Fecha:</strong> 1 de septiembre - 15 de septiembre<br />
+          <strong>Duración:</strong> 15 días<br />
+          <strong>Proyecto:</strong> Sitio web de Tortas Ahogadas Lupita
+        </p>
+
+        <h3>Objetivos y Metas</h3>
+        <ul>
+          <li>Diseñar y desarrollar la página de inicio del sitio web.</li>
+          <li>Crear un menú categorizado para los diferentes tipos de tortas.</li>
+          <li>Resaltar la autenticidad y la historia del negocio mediante diseño visual y contenido.</li>
+          <li>Integrar detalles de productos y opciones de personalización para una experiencia completa.</li>
+          <li>Asegurar un diseño responsive que funcione en dispositivos de diferentes tamaños.</li>
+        </ul>
+
+        <h3>Elementos del Backlog</h3>
+        <ul>
+          <li>ID #1 - Diseño de la página de inicio</li>
+          <li>ID #2 - Menú categorizado</li>
+          <li>ID #3 - Información de contacto y ubicación</li>
+          <li>ID #4 - Galería de imágenes</li>
+          <li>ID #5 - Integración de datos de productos</li>
+        </ul>
+
+        <h3>Plan del Sprint</h3>
+        <p>
+          El plan del sprint abarca tareas de diseño de interfaz, experiencia de usuario, desarrollo frontend y backend,
+          creación de contenido y pruebas para lograr los objetivos planteados.
+        </p>
+
+        <h3>Roles y Responsabilidades</h3>
+        <ul>
+          <li>Diseñador ui/ux - Diseño de la interfaz de usuario y experiencia de usuario.</li>
+          <li>Desarrollador Frontend - Desarrollo de la parte visible del sitio web.</li>
+          <li>Desarrollador Backend - Desarrollo de la lógica y funcionalidades detrás del sitio.</li>
+          <li>Equipo de Contenido - Creación de contenido como imágenes y textos.</li>
+          <li>Equipo de Pruebas - Realización de pruebas para garantizar la calidad del sitio.</li>
+        </ul>
+
+        <h3>Notas y Consideraciones</h3>
+        <p>
+          Es vital mantener la coherencia con la identidad de la marca en todo el diseño.
+          La colaboración estrecha entre los miembros del equipo y la comunicación constante son esenciales para el éxito del sprint.
+       <p>LINK A BOCETOS: https://codepen.io/BRAYAN-RAYMUNDO-SALAS-RODRIGUEZ/pen/XWoXPZX 
+       https://codepen.io/BRAYAN-RAYMUNDO-SALAS-RODRIGUEZ/pen/WNLrYLJ
+       </p>
+       <p>Referencias </p>
+       <p> Asana. (2022, August 17). Backlog: qué es el trabajo pendiente del sprint y ejemplos. Asana.
+https://asana.com/es/resources/sprint-backlog
+Sprint y Sprint Backlog: puntos esenciales de SCRUM. (2018, May 25).
+Blanco, J. M. (2022, July 14). Product Backlog y Sprint Backlog en Scrum. Plain Concepts.
+https://www.plainconcepts.com/es/product-backlog-sprint-backlog/
+Smartsheet. (s.f.). Las mejores plantillas de Excel para la gestión ágil de proyectos. Recuperado de
+https://es.smartsheet.com/agile-project-management-excel-templates</p>
+        </p>
+      </div>
+
+      <table>
+        <thead>
+          <tr>
+            
+          </tr>
+        </thead>
+        <tbody>
+          
+          {/* ... (resto de las filas del sprint) */}
+        </tbody>
+        
+      </table>
+    </div>
+    </table>
+  );
+}
+
+export default SprintTable;
